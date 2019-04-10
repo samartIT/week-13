e13817ac1f6ec40ae3e5b45a8ebc16d2c2a35bb5
it59070034 <42163063+it59070034@users.noreply.github.com>
2019-04-10 10:12:59 +0700

    Open/Close

---
+++ week13/Assets/Scripts/DeviceOperator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceOperator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float radius = 1.5f;
+
+   void Update()
+    {
+        if (Input.GetButtonDown("Fire3")) {
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (Collider hitCollider in hitColliders) {
+                Vector3 direction = hitCollider.transform.position - transform.position;
+                if (Vector3.Dot(transform.forward, direction) > 0.5f) {
+                    hitCollider.SendMessage("Operate", SendMessageOptions.DontRequireReceiver);
+                }
+                Debug.Log(Vector3.Dot(transform.forward, direction));
+            }
+        }
+    }
+}
