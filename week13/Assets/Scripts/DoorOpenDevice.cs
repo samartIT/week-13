eff4a90797dacefc9fde32ae5161d04455596a23
Thatsarat <59070060@it.kmitl.ac.th>
2019-04-10 13:27:58 +0700

    ถึงข้อ 5

---
+++ week13/Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;
+    private bool _open;
+    public void Operate()
+    {
+        if (_open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+        _open = !_open;
+    }
+    public void Activate() { 
+        Debug.Log("Activate");
+        if (!_open) {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+            _open = true;
+                }
+        }
+
+    public void Deactivate()
+    {
+        Debug.Log("Deactivate");
+        if (_open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+            _open = false;
+        }
+    }
+}
