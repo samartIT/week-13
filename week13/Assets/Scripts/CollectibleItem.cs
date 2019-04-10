008a55b9c34a57c50f0957f3ec0a0e1c25197b4f
OKKachasom <59070001@kmitl.ac.th>
2019-04-10 15:53:24 +0700

    not done

---
+++ week13/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+    void OnTriggerEnter()
+    {
+        Debug.Log("item collected = " + itemName);
+        Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
