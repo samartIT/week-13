51685999ff6d689631f03e860ce050c82231fe2e
it59070034 <42163063+it59070034@users.noreply.github.com>
2019-04-10 10:31:52 +0700

    Using “Trigger” enter/exit method

---
+++ week13/Assets/Scripts/DeviceTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject[] targets;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject target in targets) {
+            target.SendMessage("Activate");
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        foreach (GameObject target in targets) {
+            target.SendMessage("Deactivate");
+        }
+    }
+}
