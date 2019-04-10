0c4140cd4b2bf77673cbf2185f383436025a34b1
PeetTharathanakul <peet12131424@gmail.com>
2019-04-10 11:36:45 +0700

    test

---
+++ week13/Assets/Scripts/DoorOpenDevice.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenDevice : MonoBehaviour
+{
+    [SerializeField] private Vector3 dPos;
+    private bool _open;
+    public void Operate()
+    {
+        if (_open)
+        {
+            Vector3 pos = transform.position - dPos;
+            transform.position = pos;
+        }
+        else
+        {
+            Vector3 pos = transform.position + dPos;
+            transform.position = pos;
+        }
+        _open = !_open;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
