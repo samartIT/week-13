e317ca58428cfabc9985c3cecf73bf557e235232
FAHKRAM <59070002@it.kmitl.ac.th>
2019-04-14 23:29:16 +0700

    Lab ข้อ 3-4

---
+++ week13/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("item collected = " + itemName);
+        Destroy(this.gameObject);
+    }
+}
