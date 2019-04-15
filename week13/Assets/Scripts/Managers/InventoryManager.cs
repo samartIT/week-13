dbe7c1e0a730905d308e563930e6ebb82c0cab44
FAHKRAM <59070002@it.kmitl.ac.th>
2019-04-15 23:06:18 +0700

    ข้อ5

---
+++ week13/Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    private List<string> _items;
+
+    public void Startup() {
+        Debug.Log("Inventory manager starting...");
+
+        _items = new List<string>();
+
+        status = ManagerStatus.Started;
+    }
+
+    void DisplayItems() {
+        string itemDisplay = "Items: ";
+        foreach (string item in _items) {
+            itemDisplay += item + " ";
+        }
+        Debug.Log(itemDisplay);
+    }
+
+    public void AddItem(string name) {
+        _items.Add(name);
+        DisplayItems();
+    }
+}
