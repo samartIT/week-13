1e340dd4ee0f555f6742e6a0c69c7881c39c0fbb
Nattakit-Amonjarussang <59070048@kmitl.ac.th>
2019-04-17 06:46:23 +0700

    aaa

---
+++ week13/Assets/Scripts/BasicUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicUI : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        int posX = 10, posY = 10;
+        int width = 100, height = 30;
+        int buffer = 10;
+
+        List<string> itemList = Manager.Inventory.GetItemList();
+        if (itemList.Count == 0)
+        {
+            GUI.Box(new Rect(posX, posY, width, height), "No Item");
+        }
+        foreach (string item in itemList)
+        {
+            int count = Manager.Inventory.GetItemCount(item);
+            Texture2D image = Resources.Load<Texture2D>("Icon/" + item);
+            GUI.Box(new Rect(posX, posY, width, height), 
+                new GUIContent("(", + count + ")", image));
+            posX += width + buffer;
+        }
+
+
+        string equipped = Manager.Inventory.equippedItem;
+        if (equipped != null)
+        {
+            posX = Screen.width - (width + buffer);
+            Texture2D image = Resources.Load<Texture2D>("Icon/" + equipped);
+            GUI.Box(new Rect(posX, posY, width, height),
+                new GUIContent("Equipped", image));
+        }
+
+        posX = 10;
+        posY += height + buffer;
+
+        foreach (string item in itemList)
+        {
+            if(GUI.Button(new Rect(posX,posY,width,height),"Equip " + item))
+            {
+                Manager.Inventory.EquipItem(item);
+            }
+            if (item == "health")
+            {
+                if (GUI.Button(new Rect(posX,posY+height+buffer,width,height),"Use Health"))
+                {
+                    Manager.Inventory.ConsumeItem("health");
+                    Manager.Player.ChangeHealth(25);
+                }
+            }
+            posX += width + buffer;
+        }
+    }
+}
---
+++ week13/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("item collected = " + itemName);
+        Manager.Inventory.AddItem(itemName);
+        //Destroy(this.gameObject);
+    }
+}
