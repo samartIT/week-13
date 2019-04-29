29e5747800a1746ddd285c14db8249316b33add5
Pai <59070155@it.kmitl.ac.th>
2019-04-29 16:48:50 +0700

    item

---
+++ week13/Assets/Scripts/CollectibleItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleItem : MonoBehaviour
+{
+    [SerializeField] private string itemName;
+    // Start is called before the first frame update
+    void OnTriggerEnter()
+    {
+        Debug.Log("item collected = " + itemName);
+        Destroy (this.gameObject);
+    }
+}
